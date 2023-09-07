a487ad9b8e3c6179a472294cd353ae5ec60a5d12
Motimon <94609614+Motimon@users.noreply.github.com>
2023-09-08 00:06:51 +0800

    Delete LVGL - backup2 directory

--- LVGL - backup2/OBJ/lv_hal_tick.d
+++
@@ -1,8 +0,0 @@
-..\obj\lv_hal_tick.o: ..\LVGL\GUI\lvgl\src\hal\lv_hal_tick.c
-..\obj\lv_hal_tick.o: ..\LVGL\GUI\lvgl\src\hal\lv_hal_tick.h
-..\obj\lv_hal_tick.o: ..\LVGL\GUI\lvgl\src\hal\../lv_conf_internal.h
-..\obj\lv_hal_tick.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\lv_hal_tick.o: ..\LVGL\GUI\lvgl\src\hal\../lv_conf_kconfig.h
-..\obj\lv_hal_tick.o: ..\LVGL\GUI\lvgl\examples\porting\../../lv_conf.h
-..\obj\lv_hal_tick.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-..\obj\lv_hal_tick.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
