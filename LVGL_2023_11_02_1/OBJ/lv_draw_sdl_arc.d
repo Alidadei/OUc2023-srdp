c06cc2fb7277c28ecaa554a281cdbc1697ac0829
Motimon <liyantao@stu.ouc.edu.cn>
2023-11-02 23:35:23 +0800

    --
    --

--- LVGL_2023_11_02_1/OBJ/lv_draw_sdl_arc.d
+++
@@ -1,5 +0,0 @@
-..\obj\lv_draw_sdl_arc.o: ..\LVGL\GUI\lvgl\src\draw\sdl\lv_draw_sdl_arc.c
-..\obj\lv_draw_sdl_arc.o: ..\LVGL\GUI\lvgl\src\draw\sdl\../../lv_conf_internal.h
-..\obj\lv_draw_sdl_arc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\lv_draw_sdl_arc.o: ..\LVGL\GUI\lvgl\src\draw\sdl\../../lv_conf_kconfig.h
-..\obj\lv_draw_sdl_arc.o: ..\LVGL\GUI\lvgl\examples\porting\../../lv_conf.h
---
+++ LVGL_2023_11_02_V2/OBJ/lv_font.d
@@ -0,0 +1,16 @@
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\lv_font.c
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\lv_font.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../lv_conf_internal.h
+..\obj\lv_font.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../lv_conf_kconfig.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\examples\porting\../../lv_conf.h
+..\obj\lv_font.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+..\obj\lv_font.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\lv_symbol_def.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_area.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_utils.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_log.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_types.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_assert.h
+..\obj\lv_font.o: ..\LVGL\GUI\lvgl\src\font\../misc/lv_mem.h
+..\obj\lv_font.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
